ab7f5bdd73471872c2106b47ad784b2b1681081f
eliott PARIS <eliottparis@gmail.com>
2017-03-13 11:59:10 +0100

    Add test for valid user creation

--- test/users.js
+++ test/users.js
@@ -3,10 +3,19 @@ process.env.NODE_ENV = 'test';
 
 const test = require('tape');
 let request = require('supertest');
+
+const helper = require('./helper');
 let server = require('../app/app');
 let app = server.app;
 
+const userTest = {
+  email: 'johndoe@gmail.com',
+  password: 'password'
+}
+
 test('GET /api/users', t => {
+
+  helper.clearDatabase();
 
   request(app).get('/api/users')
   .set('Accept', 'application/json')
@@ -20,6 +29,8 @@ test('GET /api/users', t => {
 });
 
 test('POST /api/users with no info provided should fail', t => {
+
+  helper.clearDatabase();
 
   request(app).post('/api/users')
   .set('Accept', 'application/json')
@@ -36,6 +47,8 @@ test('POST /api/users with no info provided should fail', t => {
 
 test('Create a new user with only email provided should fail', t => {
 
+  helper.clearDatabase();
+
   request(app).post('/api/users')
   .set('Accept', 'application/json')
   .send({email: 'toto@gmail.com'})
@@ -44,6 +57,22 @@ test('Create a new user with only email provided should fail', t => {
   .end((err, res) => {
     t.error(err, 'request failed')
     t.ok(res.body.error === true, 'response has an error')
+    t.end()
+  });
+
+});
+
+test('POST /api/users with with valid email and password should pass', t => {
+
+  helper.clearDatabase();
+
+  request(app).post('/api/users')
+  .set('Accept', 'application/json')
+  .send(userTest)
+  .expect(200)
+  .expect('Content-Type', /json/)
+  .end((err, res) => {
+    t.error(err, 'user has been created')
     t.end()
   });
 
