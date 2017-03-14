fe34fb0cdc7d909f387f959efc08f9ad1db6c827
eliott PARIS <eliottparis@gmail.com>
2017-03-14 21:03:52 +0100

    add api documentation using apidoc

--- routes/api/api.js
+++ routes/api/api.js
@@ -10,7 +10,27 @@ const api_paths = {
   document_url:   "/documents/:id"
 }
 
-// GET /
+/**
+ * @api {get} / Request API Paths informations
+ * @apiName GetApiPaths
+ * @apiGroup Global
+ *
+ * @apiSuccess {String} auth_url Authorization path.
+ * @apiSuccess {String} users_url Path to make User request.
+ * @apiSuccess {String} user_url Path to make single User request.
+ * @apiSuccess {String} documents_url Path to make Document request.
+ * @apiSuccess {String} document_url Path to make single Document request.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *        "auth_url":       "/auth",
+ *        "users_url":      "/users",
+ *        "user_url" :      "/users/:id",
+ *        "documents_url":  "/documents",
+ *        "document_url":   "/documents/:id"
+ *     }
+ */
 router.get('/', (req, res, next) => {
   res.json(api_paths);
 });
