938c7c1bffcb89287d4bf829a10e19c152761939
jean-millot <jean.millot7@gmail.com>
2018-05-04 14:32:10 +0200

    Update documentation link.

--- app/views/index.vue
+++ app/views/index.vue
@@ -11,7 +11,7 @@
         <a class="ui huge primary button" href="https://github.com/Musicoll/Kiwi/releases"><i class="download icon"></i> Download</a>
         <a class="ui github button large" href="https://github.com/Musicoll/Kiwi"><i class="github icon"></i> Github</a>
         <a class="ui large button" href="http://musicoll.mshparisnord.org/">Site du projet Musicoll</a>
-        <a class="ui large button" href="http://musicoll.github.io/Kiwi/html/">Documentation</a>
+        <a class="ui large button" href="http://musicoll.github.io/Kiwi">Documentation</a>
 
       </div>
     </div>
